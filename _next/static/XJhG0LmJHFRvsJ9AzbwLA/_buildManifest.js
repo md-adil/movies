dc72f2ee1535089c89e99c06b3158e2762166d98
Md Adil <md-adil@live.com>
2021-10-17 11:37:04 +0530

    Updates

--- _next/static/XJhG0LmJHFRvsJ9AzbwLA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/137-654a710039219fce2c11.js","static/css/01c9a08a3e47665b825c.css","static/chunks/pages/index-b8c963106d5bdfe587a2.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
